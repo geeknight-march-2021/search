53f01d2d5ad5613f560f2a4258ce68d67b397b62
Pallavi Rawat <me@rawatpallavi.com>
2021-03-09 02:58:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import algoliasearch from 'algoliasearch/lite';
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Pagination,
+  Highlight,
+} from 'react-instantsearch-dom';
+import PropTypes from 'prop-types';
+import './App.css';
+
+const searchClient = algoliasearch(
+  'YO6PC43NQ1',
+  'ca9193c095e56259c3d1dcfd312a85b7'
+);
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header className="header">
+          <h1 className="header-title">
+            <a href="/">search-mf</a>
+          </h1>
+          <p className="header-subtitle">
+            using{' '}
+            <a href="https://github.com/algolia/react-instantsearch">
+              React InstantSearch
+            </a>
+          </p>
+        </header>
+
+        <div className="container">
+          <InstantSearch searchClient={searchClient} indexName="products">
+            <div className="search-panel">
+              <div className="search-panel__results">
+                <SearchBox
+                  className="searchbox"
+                  translations={{
+                    placeholder: '',
+                  }}
+                />
+                <Hits hitComponent={Hit} />
+
+                <div className="pagination">
+                  <Pagination />
+                </div>
+              </div>
+            </div>
+          </InstantSearch>
+        </div>
+      </div>
+    );
+  }
+}
+
+function Hit(props) {
+  return (
+    <article>
+      <h1>
+        <Highlight attribute="title" hit={props.hit} />
+      </h1>
+      <p>
+        <Highlight attribute="name" hit={props.hit} />
+      </p>
+      <p>
+        <Highlight attribute="description" hit={props.hit} />
+      </p>
+    </article>
+  );
+}
+
+Hit.propTypes = {
+  hit: PropTypes.object.isRequired,
+};
+
+export default App;
