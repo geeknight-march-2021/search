a69d398ff759f2d4dd2bc6d9ea2f6afa1c90c13c
Rohit Mulange <rohit.mulange@thoughtworks.com>
2021-03-15 12:45:41 +0530

    Add trailer youtube embed in React Modal

--- src/Hits.js
+++ src/Hits.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {
   connectHighlight,
   connectHits
@@ -9,7 +9,19 @@ import CardActions from '@material-ui/core/CardActions';
 import styled from 'styled-components';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
+import Modal from 'react-modal';
 
+const customStyles = {
+  content : {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    padding: '1rem',
+    transform: 'translate(-50%, -50%)'
+  }
+};
 
 const CustomHighlight = connectHighlight(({ highlight, attribute, hit }) => {
   const parsedHit = highlight({
@@ -54,9 +66,35 @@ function Hits ({hits}) {
   )
 };
 
+function TrailerButton({hit}) {
+  const [modalIsOpen,setIsOpen] = useState(false);
+
+  function closeModal(){
+    setIsOpen(false);
+  }
+  function showTrailer() {
+    setIsOpen(true);
+  }
+  return(
+    <>
+      <Button onClick={showTrailer} fullWidth variant="outlined" color="secondary">Watch Trailer</Button>
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <div>
+          <iframe width="560" height="315" src={hit.trailer} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </div>
+        <Button variant="contained" color="primary" onClick={closeModal}>Close</Button>
+      </Modal>
+    </>
+  )
+}
 function Hit({hit}) {
   return (
-    <Card>
+    <Card key={hit.objectID}>
       <CardContent>
         <Typography variant="h5" gutterBottom={true} component="h2">
           <CustomHighlight attribute="title" hit={hit} />
@@ -69,7 +107,7 @@ function Hit({hit}) {
         </Typography>
       </CardContent>
       <CardActions>
-        <Button fullWidth variant="outlined" color="secondary">Watch Trailer</Button>
+        <TrailerButton hit={hit}></TrailerButton>
       </CardActions>
     </Card>
   );
